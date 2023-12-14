53121c94aa90ee13107d1ffda76795e7569aff48
Swasti008 <swastimohanty452@gmail.com>
2023-12-14 17:56:12 +0530

    done

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // Progression 1: create a function and fetch the api using axios
+
+function getData() {
+    axios.get("https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=f3992e8642ddaafae4d2d950e89f0551")
+        .then(function (response) {
+            let data = response.data.articles;
+            console.log(data);
+            stored(data);
+        })
+        .catch(function (error) {
+            console.error("Sorry, we cannot fetch data due to an error", error);
+        });
+}
+
+function stored(data) {
+    data.forEach((eachData) => {
+        let container = document.getElementById('news');
+        let eachPersonDiv = document.createElement('div');
+        eachPersonDiv.classList = 'eachNews';
+
+        function createAndAppend(tag, text) {
+            let tagName = document.createElement(tag);
+            tagName.setAttribute('class', 'news');
+            if (tag === 'img') {
+                tagName.src = text;
+            } else {
+                tagName.innerText = text;
+            }
+            eachPersonDiv.appendChild(tagName);
+        }
+
+        createAndAppend('h3', eachData.title);
+        createAndAppend('img', eachData.image);
+        createAndAppend('p', eachData.content);
+        createAndAppend('span', eachData.publishedAt);
+        container.appendChild(eachPersonDiv);
+
+    });
+}
+
+getData();
